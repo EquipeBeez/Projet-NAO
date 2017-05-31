0b426d167f9ecfc619fbefd10cd82987ca236d6d
truknet <info@truknet.com>
2017-05-31 18:43:42 +0200

    SensioLabs Sup container in service Dashboard

--- src/AppBundle/Services/LoadDashboard.php
+++ src/AppBundle/Services/LoadDashboard.php
@@ -2,20 +2,19 @@
 namespace AppBundle\Services;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class LoadDashboard
 {
     private $em;
-    private $container;
     private $dashboard = array();
+
     /**
      * @param EntityManagerInterface $em
      */
-    public function __construct(EntityManagerInterface $em, ContainerInterface $container)
+    public function __construct(EntityManagerInterface $em, $var_project)
     {
         $this->em = $em;
-        $this->container = $container;
+        $this->var_project = $var_project;
     }
 
 
@@ -27,9 +26,9 @@ class LoadDashboard
             'member_moderator' => count($this->em->getRepository('UserBundle:User')->findUserByRole('ROLE_MODERATEUR')),
             'member_admin' => count($this->em->getRepository('UserBundle:User')->findUserByRole('ROLE_SUPER_ADMIN')),
             'count_observation' => count($this->em->getRepository('AppBundle:Observation')->findAll()),
-            'count_observation_valid' => count($this->em->getRepository('AppBundle:Observation')->findby(array('status' => $this->container->getParameter('var_project')['status_obs_valid']))),
-            'count_observation_waiting' => count($this->em->getRepository('AppBundle:Observation')->findby(array('status' => $this->container->getParameter('var_project')['status_obs_waiting']))),
-            'count_observation_reject' => count($this->em->getRepository('AppBundle:Observation')->findby(array('status' => $this->container->getParameter('var_project')['status_obs_rejeted']))),
+            'count_observation_valid' => count($this->em->getRepository('AppBundle:Observation')->findby(array('status' => $this->var_project['status_obs_valid']))),
+            'count_observation_waiting' => count($this->em->getRepository('AppBundle:Observation')->findby(array('status' => $this->var_project['status_obs_waiting']))),
+            'count_observation_reject' => count($this->em->getRepository('AppBundle:Observation')->findby(array('status' => $this->var_project['status_obs_rejeted']))),
             'count_species' => count($this->em->getRepository('AppBundle:Taxrefv10')->findAll()),
         );
         return $this->dashboard;
